0c4e6c0ba54d093033672ba6bd8ee64e47bbc5b4
leeyh-tobesystem <leeyh@tobesystem.co.kr>
2022-12-09 13:28:40 +0900

    first commit

---
+++ src/main/java/com/project/base/BaseApplication.java
@@ -0,0 +1,17 @@
+package com.project.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+public class BaseApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/project/base/Controller/MainController.java
@@ -0,0 +1,19 @@
+package com.project.base.Controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+@Slf4j
+public class MainController {
+
+    @RequestMapping(value = "/")
+    public String index(HttpServletRequest req, HttpServletResponse res) {
+        System.out.println("test");
+        return "index";
+    }
+}
